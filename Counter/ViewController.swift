b5e6dc287737a720492a08c5c0822324de041ddd
Kalmykov Iaroslav <kalmykov-iap@yandex.ru>
2023-11-17 22:20:43 +0900

    Создание счетчика.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var counter = 0
+    
+    @IBAction func counterIncrease(_ sender: Any) {
+        counter += 1
+        text.text = "\(counter)"
+    }
+    
+    @IBAction func counterZero(_ sender: Any) {
+        counter = 0
+        text.text = "\(counter)"
+    }
+    
+    @IBAction func counterDecrease(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            text.text = "\(counter)"
+        } else {
+            text.text = "В минус не уйдем"
+        }
+    }
+    
+    
+    @IBOutlet weak var buttonIncrease: UIButton!
+    @IBOutlet weak var buttonZero: UIButton!
+    @IBOutlet weak var buttonDecrease: UIButton!
+    @IBOutlet weak var text: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
