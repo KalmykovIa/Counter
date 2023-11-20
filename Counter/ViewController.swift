e3691dad103e8875a84d11f209924f17e238593f
Kalmykov Iaroslav <kalmykov-iap@yandex.ru>
2023-11-20 18:54:18 +0900

    Выполнены условия базовой задачи.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,37 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     private var counter = 0
     
     @IBAction func counterIncrease(_ sender: Any) {
         counter += 1
-        text.text = "\(counter)"
+        text.text = "Значение счетчика: \(counter)"
     }
-    
-    @IBAction func counterZero(_ sender: Any) {
-        counter = 0
-        text.text = "\(counter)"
-    }
-    
-    @IBAction func counterDecrease(_ sender: Any) {
-        if counter > 0 {
-            counter -= 1
-            text.text = "\(counter)"
-        } else {
-            text.text = "В минус не уйдем"
-        }
-    }
-    
-    
     @IBOutlet weak var buttonIncrease: UIButton!
-    @IBOutlet weak var buttonZero: UIButton!
-    @IBOutlet weak var buttonDecrease: UIButton!
     @IBOutlet weak var text: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
-
 }
 
