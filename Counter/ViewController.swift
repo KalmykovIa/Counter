952eda72daa06904355c2e5a429a5023d77d78c5
Kalmykov Iaroslav <kalmykov-iap@yandex.ru>
2023-11-21 21:59:19 +0900

    Добавлено: Вывод значения в скролле, дата и время в выводе.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,17 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     private var counter = 0
+    @IBOutlet private weak var scrollView: UIScrollView!
     
-    @IBAction func counterIncrease(_ sender: Any) {
+    @IBOutlet private weak var textView: UITextView!
+    
+    @IBAction private func counterIncrease(_ sender: Any) {
         counter += 1
-        text.text = "Значение счетчика: \(counter)"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let currentDate = Date()
+        let formattedDate = dateFormatter.string(from: currentDate)
+        textView.text += "\(formattedDate): Значение счётчика: \(counter)\n"
+        let range = NSMakeRange(textView.text.count - 1, 1)
+        textView.scrollRangeToVisible(range)
     }
-    @IBOutlet weak var buttonIncrease: UIButton!
-    @IBOutlet weak var text: UILabel!
+    @IBOutlet private weak var buttonIncrease: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        textView.isEditable = false
+        textView.isScrollEnabled = true
+        scrollView.addSubview(textView)
+        view.addSubview(scrollView)
+        
     }
 }
 
